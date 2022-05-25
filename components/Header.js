7193979af80335c89ee1e7cca74a2bdef12a2b95
Steve Clark <steve.a.clark48@gmail.com>
2022-05-25 10:08:19 -0500

    Added import for connect button

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
 import Search from '../assets/svg/search'
+import { ConnectButton } from 'web3uikit'
+
+
 const styles = {
     header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
     headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
@@ -55,7 +58,7 @@ const Header = () => {
             </nav>
 
             <div className='flex items-center'>
-                {/* <ConnectButton/> */}
+                {<ConnectButton/>}
                 <div className={styles.inputContainer}>
                     {<Search />}
                     <input className={styles.input} placeHolder = 'Search'/>
