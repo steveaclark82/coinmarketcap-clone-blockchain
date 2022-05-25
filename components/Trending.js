6bca8edbcbe12c1d97fc0ea4a9d1ca8590ecc925
Steve Clark <steve.a.clark48@gmail.com>
2022-05-25 10:21:43 -0500

    Added imports

--- components/Trending.js
+++ components/Trending.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState} from 'react'
+import fire from '../assets/fire.png'
+import btc from '../assets/btc.png'
+import usdt from '../assets/usdt.png'
+import gainers from '../assets/gainers.png'
+import recent from '../assets/recent.png'
 
 export const Trending = () => {
   return (
