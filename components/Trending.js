179f0640ade54c4426d6a8e6b641e13cb9a8dc0a
Steve Clark <steve.a.clark48@gmail.com>
2022-05-25 10:36:26 -0500

    Added styling and headers

--- components/Trending.js
+++ components/Trending.js
@@ -6,12 +6,26 @@ import gainers from '../assets/gainers.png'
 import recent from '../assets/recent.png'
 import ReactSwitch from 'react-switch'
 
+const styles = {
+    trendingWrapper: `mx-auto-w-screen-2xl`,
+    h1: 'text-3xl text-white'
+}
+
 const Trending = () => {
     const [checked, setChecked] = useState(false)
 
   return (
-    <div>
+    <div className='text-white'>
+        <div className={styles.trendingWrapper}>
+            <div className='flex justify-between'>
+                <h1 className={styles.h1}>Todays Cryptocurrency Prices by Market Cap</h1>
 
+                <div className='flex'>
+                    <p className='text-gray-400'>Highlights &nbsp</p>
+                    <ReactSwitch checked={checked} onChange={()=> {setChecked(!checked)}}/>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
