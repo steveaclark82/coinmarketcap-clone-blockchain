aa88736285e341681b3d77f8fd8aace70e8cbc7e
Steve Clark <steve.a.clark48@gmail.com>
2022-06-02 09:41:59 -0500

    Added style and imports

--- components/TrendingCardRow.js
+++ components/TrendingCardRow.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import Rate from './cmc-table/Rate'
+import Image from 'next/image'
 
-export const TrendingCardRow = () => {
+const styles = {
+    trendingCardRow: `flex items-center justify-between mb-4 text-[0.93rem]`,
+}
+
+const TrendingCardRow = () => {
   return (
     <div>TrendingCardRow</div>
   )
