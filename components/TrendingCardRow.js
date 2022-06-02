5a299e8ae717c59ad98798f32767100a4ec01d76
Steve Clark <steve.a.clark48@gmail.com>
2022-06-02 09:52:34 -0500

    Added div and p tags

--- components/TrendingCardRow.js
+++ components/TrendingCardRow.js
@@ -6,9 +6,21 @@ const styles = {
     trendingCardRow: `flex items-center justify-between mb-4 text-[0.93rem]`,
 }
 
-const TrendingCardRow = ({number, icon, name, isIncrement, rate}) => {
+const TrendingCardRow = ({number, icon, name, symbol, isIncrement, rate}) => {
   return (
-    <div>TrendingCardRow</div>
+    <div className={styles.trendingCardRow}>
+        <p className='opacity-40'>{number}</p>
+        <div className='w-full flex'>
+            <div className='mx-5'>
+                {icon && <Image src={icon} width={20} height={20}/>}
+            </div>
+            <p className='font-bold'>
+                {name}
+                <span className='text-gray-400'>{symbol}</span>
+            </p>
+        </div>
+        <Rate isIncrement={isIncrement} rate={rate} />
+    </div>
   )
 }
 
